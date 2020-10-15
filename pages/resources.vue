36c631243ec0406398c1cc2dcde286747af693f7
Joseph Hornby <joehornby@gmail.com>
2020-10-15 22:19:24 +0100

    add resources page

---
+++ pages/resources.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="section light">
+    <div class="page-content grid-container grid-container__two-col">
+      <div class="heading heading--left">
+        <h1>{{ page.fields.title }}</h1>
+      </div>
+    </div>
+    
+    <article class="page-content grid-container grid-container__two-col" v-html="richTextHtml"></article>
+  </div>
+</template>
+
+<script>
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer"
+
+  export default {
+    data() {
+      return {
+        slug: "resources"
+      }
+    },
+    computed: {
+      
+      page() {
+        let page = this.$store.state.pages.find(
+          el => el.fields.slug === this.slug
+        )
+        return page
+      },
+      richTextHtml() {
+        let richTextHtml = documentToHtmlString(this.page.fields.content)
+        return richTextHtml
+      },
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
