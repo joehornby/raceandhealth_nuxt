c340188c18a5e1dd75355e048980f1889c7fde38
Joseph Hornby <joehornby@gmail.com>
2020-10-15 22:25:18 +0100

    add footer signup

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="footer dark">
     <div class="grid-container grid-container__two-col">
+      <!-- Begin Mailchimp Signup Form -->
+
+      <div id="mc_embed_signup">
+        <form action="https://raceandhealth.us2.list-manage.com/subscribe/post?u=aeab444f69b595499b9019150&amp;id=7f5bdb1ed8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <div id="mc_embed_signup_scroll">
+            <label for="mce-EMAIL">Subscribe to The Race &amp; Health Quarterly to follow our latest events, publications, and thoughts.</label>
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_aeab444f69b595499b9019150_7f5bdb1ed8" tabindex="-1" value=""></div>
+            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn">
+          </div>
+        </form>
+      </div>
+      <!--End mc_embed_signup-->
+    </div>
+    <div class="grid-container grid-container__two-col">
+      <div class="footer__links">
+        <nuxt-link to="/work-with-us">
+          Work with us
+        </nuxt-link>
+      </div>
       <div class="footer__social">
         <a href="https://twitter.com/raceandhealth" title="twitter">
           <i class="fab fa-twitter"></i>
@@ -19,8 +40,8 @@
         </a>
       </div>
       <div class="footer__legal">
-        <a href="privacy-policy.html">Privacy Policy</a>
-        <a href="contact.html">Contact</a>
+        <nuxt-link to="/privacy-policy">Privacy Policy</nuxt-link>
+        <nuxt-link to="/contact">Contact</nuxt-link>
       </div>
     </div>
   </div>
@@ -45,28 +66,33 @@ export default {
     z-index: 100;
     grid-column: 1 / span 1;
   }
-  .footer__legal {
+  .footer__legal,
+  .footer__links {
     grid-column: 2 / span 1;
     align-self: flex-end;
     right: 0;
   }
 }
 
-.footer__social,
-.footer__legal {
-  padding-bottom: 1rem;
+.footer {
+  &__social,
+  &__legal,
+  &__links
+   {
+    padding-bottom: 1rem;
 
-  a {
-    display: inline-block;
-    margin-right: 3em;
-    color: $color-cream;
-    position: relative;
+    a {
+      display: inline-block;
+      margin-right: 3em;
+      color: $color-cream;
+      position: relative;
 
-    text-decoration: none;
-
-    &:hover {
-      color: $color-red--darker;
       text-decoration: none;
+
+      &:hover {
+        color: $color-red--darker;
+        text-decoration: none;
+      }
     }
   }
 }
@@ -85,5 +111,47 @@ export default {
       content: none;
     }
   }
+}
+
+/* MailChimp Email Signup Form */
+.footer {
+  #mc_embed_signup {
+    margin-top: 2rem;
+  }
+
+  label {
+    display: block;
+    max-width: 35ch;
+  }
+
+  input[type="email"] {
+    display: block;
+    background-color: $color-darkgrey;
+    color: $color-cream;
+    width: 100%;
+    margin-top: 1rem;
+    padding: 0.75rem 1rem;
+    border: 3px solid $color-cream;
+  }
+
+  input[type="submit"] {
+    background-color: transparent;
+    border-radius: 50px;
+    border-color: $color-cream;
+
+    &:placeholder {
+      color: rgba($color-cream, 1);
+    }
+
+    &:hover {
+      color: $color-darkgrey;
+      background-color: $color-cream;
+    }
+  }
+}
+
+#mc-embedded-subscribe {
+  margin: 0;
+  margin-top: 2rem;
 }
 </style>
