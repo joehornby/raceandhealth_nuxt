1ace53d6cb9624f2c7db5f5a3855ab5ffb74f941
Joseph Hornby <joehornby@gmail.com>
2020-10-15 22:19:14 +0100

    add language child page

---
+++ pages/index/our-vision/_lang.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="content--right vision">
+    <p>{{ language.fields.vision }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    transition: "",
+    computed: {
+      language() {
+        let language = this.$store.state.vision.find(
+          el => el.fields.slug === this.$route.params.lang
+        )
+        return language
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .vision {
+    min-height: 40vh;
+  }
+</style>
