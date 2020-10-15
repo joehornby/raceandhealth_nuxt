71c13855c903a4463cf13aca342ac72554980a1b
Joseph Hornby <joehornby@gmail.com>
2020-10-15 22:26:05 +0100

    Revert "remove page slug parameter"
    This reverts commit 9491664786f1521372df6b4269639b248f2e7246.

---
+++ pages/index/_slug.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="content--right vision">
+    <p>{{ vision.fields.vision }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["vision"],
+    trasition: ""
+  }
+</script>
+
+<style lang="scss" scoped>
+  .vision {
+    min-height: 40vh;
+  }
+</style>
