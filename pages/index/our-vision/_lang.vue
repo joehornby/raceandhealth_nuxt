d02a167935293c5d03ec19d4896b6a6dafe93a2e
Joseph Hornby <joehornby@gmail.com>
2020-10-16 16:05:49 +0100

    add slide and height transition for Vision language change

--- pages/index/our-vision/_lang.vue
+++ pages/index/our-vision/_lang.vue
@@ -6,7 +6,30 @@
 
 <script>
   export default {
-    transition: "",
+    data() {
+      return {
+        prevHeight: 0
+      }
+    },
+    transition: {
+      name: "lang",
+      mode: "out-in",
+      beforeLeave(el) {
+        this.prevHeight = getComputedStyle(el).height;
+      },
+      enter(el){
+        const { height } = getComputedStyle(el);
+
+        el.style.height = this.prevHeight;
+
+        setTimeout(() => {
+          el.style.height = height;
+        });
+      },
+      afterEnter(el){
+        el.style.height = 'auto';
+      }
+    },
     computed: {
       language() {
         let language = this.$store.state.vision.find(
@@ -14,12 +37,25 @@
         )
         return language
       }
-    }
+    },
   }
 </script>
 
 <style lang="scss" scoped>
   .vision {
-    min-height: 40vh;
+    margin-bottom: 2rem;
+  }
+  /* Transitions */
+  .lang-enter-active,
+  .lang-leave-active {
+    transition-duration: 0.3s;
+    transition-property: height, opacity, transform;
+    transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
+    overflow: hidden;
+  }
+  .lang-enter,
+  .lang-leave-active {
+    opacity: 0;
+    transform: translate(2em, 0);
   }
 </style>
