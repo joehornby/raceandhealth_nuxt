b61e0bfb8220c2f07bffcb9d8469e07ecfc03628
Joseph Hornby <joehornby@gmail.com>
2020-10-16 22:10:27 +0100

    add dynamic route for resource category

---
+++ pages/resources/_category.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="page-content grid-container grid-container__two-col">
+      <div class="heading heading--left   ">
+        <a :id="category.fields.slug" class="anchor"></a>
+        <h2>{{ category.fields.category }}</h2>
+      </div>
+      <div class="content--right" v-html="html(category.fields.resources)">
+      </div>
+    </div>
+</template>
+
+<script>
+import { documentToHtmlString } from "@contentful/rich-text-html-renderer"
+
+export default {
+  computed: {
+    category() {
+      let category = this.$store.state.resources.find(
+        el => el.fields.slug === this.$route.params.category
+      )
+      return category
+    }
+  },
+  methods: {
+    html(doc) {
+      return documentToHtmlString(doc)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
