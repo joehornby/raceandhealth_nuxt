94ce6b293d452aaf859e7de0eba4414a769c7f25
Joseph Hornby <joehornby@gmail.com>
2020-10-16 22:09:54 +0100

    move resources to resources/index

--- pages/resources/index.vue
+++ pages/resources/index.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="section light">
     <div class="page-content grid-container grid-container__two-col">
-      <div class="heading heading--left">
+      <div class="heading heading--full">
         <h1>{{ page.fields.title }}</h1>
       </div>
+      <div class="content content--full" v-html="pageHtml"></div>
+      <nuxt-link to="resources/all">View all</nuxt-link>
     </div>
+    <nuxt-child v-for="category in resources" :key="category.fields.slug" />
     
-    <article class="page-content grid-container grid-container__two-col" v-html="richTextHtml"></article>
   </div>
 </template>
 
@@ -20,17 +22,19 @@ import { documentToHtmlString } from "@contentful/rich-text-html-renderer"
       }
     },
     computed: {
-      
+      resources() {
+        return this.$store.state.resources
+      },
       page() {
         let page = this.$store.state.pages.find(
           el => el.fields.slug === this.slug
         )
         return page
       },
-      richTextHtml() {
+      pageHtml() {
         let richTextHtml = documentToHtmlString(this.page.fields.content)
         return richTextHtml
-      },
+      }
     }
   }
 </script>
